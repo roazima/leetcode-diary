ad33eb53af2301b19fd960c10ad354c3d6e142b7
razima <razima@asu.edu>
2025-05-05 13:44:37 -0700

    update

--- valid_anagram_242.py
+++ valid_anagram_242.py
@@ -12,10 +12,7 @@ from collections import Counter
 class Solution:
    def isAnagram(self, s: str, t: str) -> bool:
         return Counter(t) == Counter(s)
-        
-
-             
-
+      
 if __name__ == "__main__":
     test_cases = [
         ("anagram", "nagaram"),
